6f3c8c730abd6cb542b298e37a74f834f67993d9
AleSuarez <alemsuarez@gmail.com>
2025-08-23 23:33:42 -0300

    se agrega middleware y se da de alta producto ok

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,3 +1,4 @@
+import { faSlash } from "@fortawesome/free-solid-svg-icons"
 import { Products } from "../models/Products.js"
 
 
@@ -7,7 +8,7 @@ export const getProducts = async (req, res) => {
     try {
         // si no viene nada trae todo, el regexp permite busquedas tipo que contenga x
         const QueryRegExp = query.name ? {name: new RegExp(query.name, "i")} : undefined
-        console.info("Se recibe params: ", query)
+        //console.info("Se recibe params: ", query)
         const products = await Products.find(QueryRegExp)
 
         res.json({
@@ -24,12 +25,12 @@ export const getProducts = async (req, res) => {
 }
 
 export const getProductsById = async (req, res) => {
-    const { params: {id}, body } = req;
+    const { id, ...body }  = req.query
     // obtengo el id para la busqueda
     try {
         
         const Query = id ? id : undefined
-        console.info("Se recibe params: ", id)
+        //console.info("Se recibe params: ", Query.id)
         const products = await Products.findById(Query)
 
         res.json({
@@ -43,6 +44,36 @@ export const getProductsById = async (req, res) => {
                 msg: "Producto no encontrado"             
             })
     }
-
-
 }
+
+export const createProduct = async (req, res) => {
+
+    const { body }  = req
+    //console.info("createProduct: ", body)
+    try {
+
+        const prod = await Products.findOne({name: body.name})
+        if (prod) {
+            console.error("Producto duplicado")
+             return res.status(409).json({
+                ok: false,
+                msg: "El producto ya existe"
+            })
+        }
+
+        const newProd = await Products.create(body)
+
+        return res.json({
+            ok: true,
+            msg: "Producto creado Correctamente",
+            newProd
+        })
+
+    } catch (error) {
+          return res.status(500).json({
+                ok: false,
+                msg: `createProduct ${error}`
+            })
+    }
+}
+
